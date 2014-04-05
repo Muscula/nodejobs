738015998f1afd4ac1b3fdacf824c1f44c9c27d2
Anton Syrovatka <syrovatka.anton@gmail.com>
2014-04-05 20:35:23 +0200

    singup

--- public/js/app.js
+++ public/js/app.js
@@ -3,14 +3,18 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives','ngRoute']).
-  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider.
-      when('/', {
-        //templateUrl: 'partials/index',
-        //controller: JobController
-      }).
-      otherwise({
-        redirectTo: '/'
-      });
-    $locationProvider.html5Mode(true);
-  }]);
+	config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+		$routeProvider.
+		when('/', {
+			templateUrl: 'home.html',
+			controller: AppCtrl
+		}).
+		when('/signup', {
+			templateUrl: 'signup.html',
+			controller: UserController
+		}).
+		otherwise({
+			redirectTo: '/'
+		});
+		$locationProvider.html5Mode(true);
+	}]);
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,22 +2,37 @@
 
 /* Controllers */
 
-function AppCtrl($scope, $http) {
-  $http.get('/jobs').success(function(jobs) {
-    $scope.jobs = jobs;
-  }).
-  error(function(data, status, headers, config) {
-    $scope.name = 'Error!';
-  });
+function AppCtrl($scope, $http, $location) {
+	$http.get('/jobs').success(function(jobs) {
+		$scope.jobs = jobs;
+	})
+	.error(function(data, status, headers, config) {
+		$scope.name = 'Error!';
+	});
+
+	$scope.go = function ( path ) {
+		$location.path(path);
+	};
 }
 
 function JobController($scope, $http) {
 	$scope.job = {};
-    $scope.createJob = function() {
-        $http({
-            method : 'POST',
-            url : '/jobs',
-            data : $scope.job
-        });
-    };
+	$scope.createJob = function() {
+		$http({
+			method : 'POST',
+			url : '/jobs',
+			data : $scope.job
+		});
+	};
+}
+
+function UserController($scope, $http) {
+	$scope.user = {};
+	$scope.createUser = function() {
+		$http({
+			method : 'POST',
+			url : '/signup',
+			data : $scope.user
+		});
+	};
 }
